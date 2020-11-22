2c7adaac1bc488f22a9a8189344951a314d36400
Thomas Smith <thsm@umich.edu>
2020-11-21 23:39:30 -0500

    Improve comments in __main__

--- src/__main__.py
+++ src/__main__.py
@@ -9,6 +9,7 @@ from .library import ShibbolethError
 
 def run():
     """Authenticate via U-M Shibboleth from the command line."""
+    # Argument parsing
     parser = argparse.ArgumentParser(
         description="Authenticate to U-M Shibboleth from the command line."
     )
@@ -21,26 +22,26 @@ def run():
     args = parser.parse_args()
     cookie_file = args.cookie_file
 
+    # Perform authentication
     try:
         cli = CLI(cookie_file)
         request = requests.Request("GET", "https://weblogin.umich.edu/")
         result = cli.perform(request)
-        #print(result.text)
         return 0
     except requests.exceptions.ConnectionError as e:
         print("Error connecting to Shibboleth server(s):", file=sys.stderr)
         return 1
     except requests.exceptions.Timeout:
         print("A request timed out.", file=sys.stderr)
-        return 1
+        return 2
     except requests.exceptions.TooManyRedirects:
         print("Too many redirects.", file=sys.stderr)
-        return 1
+        return 3
     except ShibbolethError as e:
         print(e, file=sys.stderr)
-        return 1
+        return 4
     except KeyboardInterrupt:
-        return 1
+        return 130
 
 if __name__ == "__main__":
     sys.exit(run())
